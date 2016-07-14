22d29b54eb228b65427124e466da33aa96e0216d
the-widget <brennenawana108ny@gmail.com>
2016-07-14 11:50:49 -0400

    successfully hit YouTube API in console.log

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar'
+import YouTubeSearch from 'youtube-api-search'
 const API_KEY = 'AIzaSyDCzZDGl_kXwKeAQDo4j3UDlLqsSESaZQE'
 
+YouTubeSearch({key: API_KEY, term: 'cat'}, function(data) {
+  console.log(data)
+});
 
 const App = () => {
   return (
     <div>
       <SearchBar/>
-      
+
     </div>
   );
 }
 
-
-
 ReactDOM.render(<App />, document.querySelector('.container'));
+
+
+// Create a new component.  This component should produce some HTML
+// Take this component's generated HTML and put it on the page (in the DOM).
