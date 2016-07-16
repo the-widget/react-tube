fb536c615c335e4b1dcc262199b06ccc601cdf3d
the-widget <brennenawana108ny@gmail.com>
2016-07-16 17:50:07 -0400

    promoted App to class component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar'
 import YouTubeSearch from 'youtube-api-search'
 const API_KEY = 'AIzaSyDCzZDGl_kXwKeAQDo4j3UDlLqsSESaZQE'
 
-YouTubeSearch({key: API_KEY, term: 'cat'}, function(data) {
-  console.log(data)
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
+    YouTubeSearch({key: API_KEY, term: 'cat'}, function(data) {
+      console.log(data)
+    });
+  }
 
-    </div>
-  );
+  render(){
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
-
 
 // Create a new component.  This component should produce some HTML
 // Take this component's generated HTML and put it on the page (in the DOM).
