a6f1bc4397b144661fb42c1dfb8746652f03a637
the-widget <brennenawana108ny@gmail.com>
2016-07-16 19:15:27 -0400

    added markup to video list item

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,7 @@ import VideoListItem from './video_list_item'
 
 const VideoList = (props) => {
     const VideoItems = props.videos.map( (video) => {
-      return <VideoListItem video = {video} />
+      return <VideoListItem key={video.etag} video = {video} />
     });
     
   return (
