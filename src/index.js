ead743153226cd8ff4c3f7847c287e1a3db5b8fe
the-widget <brennenawana108ny@gmail.com>
2016-07-16 20:14:44 -0400

    user can select a video

--- src/index.js
+++ src/index.js
@@ -9,10 +9,16 @@ const API_KEY = 'AIzaSyDCzZDGl_kXwKeAQDo4j3UDlLqsSESaZQE';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YouTubeSearch({key: API_KEY, term: 'cat'}, (videos) => {
-      this.setState({videos: videos});
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -20,8 +26,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar/>
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
       </div>
     );
   }
